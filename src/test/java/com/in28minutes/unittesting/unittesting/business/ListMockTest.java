8b67bf74195cf06e103a9f46ed666157965be126
antocecere <antocecere77@gmail.com>
2019-12-02 06:48:54 +0100

    Mock with multiple parameter value

---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -0,0 +1,36 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    private List mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void size_DifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+        assertNull(mock.get(1));
+    }
+
+}
