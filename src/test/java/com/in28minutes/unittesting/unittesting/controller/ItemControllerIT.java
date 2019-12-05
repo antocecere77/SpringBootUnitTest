835b2d833fa1164777105baff91a0fd4b6f395e5
antocecere <antocecere77@gmail.com>
2019-12-05 07:27:54 +0100

    Added @MockBean note

--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
@@ -1,11 +1,13 @@
 package com.in28minutes.unittesting.unittesting.controller;
 
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +17,10 @@ public class ItemControllerIT {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    //Used for mock internal service
+    //@MockBean
+    //private ItemRepository repository;
 
     @Test
     public void contextLoad() throws JSONException {
