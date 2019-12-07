ebc17dd7ef2fdd04774c856e95a64f1b5bd39e86
antocecere <antocecere77@gmail.com>
2019-12-07 11:43:08 +0100

    Added added lambda for calculate sum method

--- src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -2,16 +2,16 @@ package com.in28minutes.unittesting.unittesting.business;
 
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class SomeBusinessImpl {
 
     private SomeDataService someDataService;
 
     public int calculateSum(int[] data) {
-        int sum = 0;
-        for(int value: data) {
-            sum += value;
-        }
-        return sum;
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
     }
 
     public int calculateSumUsingDataService() {
